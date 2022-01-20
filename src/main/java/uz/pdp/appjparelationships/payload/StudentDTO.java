a8c336cf7055b2f012312a241986054479ef1434
rahmatilla <rahmatilla301993@gmail.com>
2022-01-20 18:44:53 +0500

    Commited

--- src/main/java/uz/pdp/appjparelationships/payload/StudentDTO.java
+++ src/main/java/uz/pdp/appjparelationships/payload/StudentDTO.java
@@ -1,4 +1,22 @@
 package uz.pdp.appjparelationships.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.pdp.appjparelationships.entity.Address;
+import uz.pdp.appjparelationships.entity.Group;
+import uz.pdp.appjparelationships.entity.Subject;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDTO {
+
+    private String firstName;
+    private String lastName;
+    private Integer address_id;
+    private Integer group_id;
+    private List<Integer> subjects_ids;
 }
